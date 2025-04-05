2291d62d39610daac1bad1a0db5ac0b529aba41c
Salmane Issa <143265121+Sgissa@users.noreply.github.com>
2025-04-05 04:07:32 +0000

    google login

---
+++ loginapp/app/index.tsx
@@ -0,0 +1,67 @@
+import { Text, View } from "react-native";
+
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// import firebaseConfig from "../../firebase-creds.json";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBggon2o3HVbcZUK1OtbWanV4VAPj0gsbc",
+  authDomain: "loginapp-80ea7.firebaseapp.com",
+  projectId: "loginapp-80ea7",
+  storageBucket: "loginapp-80ea7.firebasestorage.app",
+  messagingSenderId: "68604417218",
+  appId: "1:68604417218:web:375310964b162f537ad75b",
+  measurementId: "G-V1N8NVQ93W"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+import { getFirestore, collection, getDocs } from "firebase/firestore";
+
+const db = getFirestore(app);
+const testGangCollection = collection(db, 'test-gang');
+
+import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
+
+const auth = getAuth(app);
+const provider = new GoogleAuthProvider();
+provider.addScope('https://www.googleapis.com/auth/contacts.readonly');
+
+
+export default function Index() {
+
+  function doSomething() {
+    getDocs(testGangCollection).then(r=> {
+      console.log(r.docs.length);
+      for(let i = 0; i < r.docs.length; i++) {
+        const currentDoc = r.docs[i];
+        console.log(currentDoc.id, currentDoc.data());
+      }
+    }).catch (e=> {
+      console.warn("error", e);
+    })
+  }
+  function signIn() {
+    signInWithPopup(auth, provider).then(r=> {
+      console.log(r.user);
+    }).catch (e=> {
+      console.warn("error", e);
+    })
+  }
+  return (
+    <View
+      style={{
+        backgroundColor: "grey",
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Text>Edit app/index.tsx to edit this screen.</Text>
+      <button onClick={doSomething}>BTN type shh</button>
+      <button onClick={signIn}>Sign In</button>
+    </View>
+  );
+}
